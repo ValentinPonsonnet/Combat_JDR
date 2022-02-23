24bbb93df351c245e76f5e792ed5cf386daf1b44
ValentinPonsonnet <Ponsonnetvalentin@gmail.com>
2022-02-23 13:40:48 +0100

    construction du voleur

--- Traitement/traitement.php
+++ Traitement/traitement.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="traitement.php">Rafraichir</a>
-</body>
-</html>
+<?php 
+    include_once ("../model/voleur.php");
+    new Voleur("Ponsonnet","Valentin");
+?>
